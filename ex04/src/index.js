5b612fc37d7ccea739902b19e6ceabdcbcf2b9f0
zivojinminic <zivojinminic05@gmail.com>
2021-09-20 13:33:52 +0200

    Local scope

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,7 +1,7 @@
 // Only change code below this line
 function localScope() {
     var myVariable = "I am here!";
-    console.log(myVariable);
+    console.log("localScope: " + myVariable);
 }
 
 localScope();
