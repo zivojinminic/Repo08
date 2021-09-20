fdc35b1f05d5b93091c14f5776ded4f8aaf71568
zivojinminic <zivojinminic05@gmail.com>
2021-09-20 20:51:11 +0200

    Convert hexadecimal number to an integer

---
+++ ex15/src/index.js
@@ -0,0 +1,9 @@
+// Only change code below this line
+function convertToInt(str) {
+    return console.log(parseInt(str, 16));
+}
+convertToInt("BA");
+convertToInt("F1");
+convertToInt("JeffBezos");
+// Only change code above this line
+module.exports = convertToInt;
