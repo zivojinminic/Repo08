eaed6df55a85015b197e4e6ae0a35f2307ea5632
zivojinminic <zivojinminic05@gmail.com>
2021-09-26 22:15:09 +0200

    Generate Random Whole Numbers within a Range

--- ex13/src/index.js
+++ ex13/src/index.js
@@ -1,7 +1,7 @@
 // Only change code below this line
 function randomRangeNumber(minNumber, maxNumber) {
-    return console.log(Math.floor(Math.random() * (maxNumber - minNumber +1)) + minNumber);
+    return Math.floor(Math.random() * (maxNumber - minNumber +1)) + minNumber;
 }
-randomRangeNumber(5,25);
+console.log(randomRangeNumber(5,25));
 // Only change code above this line
 module.exports = randomRangeNumber;
