d34fa570341fb38e42b7dfad5ba4137b2307c663
zivojinminic <zivojinminic05@gmail.com>
2021-09-26 22:18:37 +0200

    The parseInt()

--- ex14/src/index.js
+++ ex14/src/index.js
@@ -1,9 +1,9 @@
 // Only change code below this line
 function convertStrToInt(str) {
-    return console.log(parseInt(str));
+    return parseInt(str);
 }
-convertStrToInt("44");
-convertStrToInt("27");
-convertStrToInt("JeffBezos");
+console.log(convertStrToInt("44"));
+console.log(convertStrToInt("27"));
+console.log(convertStrToInt("JeffBezos"));
 // Only change code above this line
 module.exports = convertStrToInt;
