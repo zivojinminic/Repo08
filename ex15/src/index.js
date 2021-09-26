41a44f1447b8fe471e4985f2545a633ebe178f5e
zivojinminic <zivojinminic05@gmail.com>
2021-09-26 22:22:56 +0200

    parseInt Function with a Radix

--- ex15/src/index.js
+++ ex15/src/index.js
@@ -1,9 +1,9 @@
 // Only change code below this line
 function convertToInt(str) {
-    return console.log(parseInt(str, 16));
+    return parseInt(str, 16);
 }
-convertToInt("BA");
-convertToInt("F1");
-convertToInt("JeffBezos");
+console.log(convertToInt("BA"));
+console.log(convertToInt("F1"));
+console.log(convertToInt("JeffBezos"));
 // Only change code above this line
 module.exports = convertToInt;
